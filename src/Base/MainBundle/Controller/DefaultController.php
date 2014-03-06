97c6fe87d999d3f8586c7feb052ca37fb7b845fb
Gediminas Šaltenis <gediminas@saltenis.lt>
2014-03-06 16:57:40 +0200

    user menu

--- src/Base/MainBundle/Controller/DefaultController.php
+++ src/Base/MainBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="base_main_default_index")
      * @Template()
      */
     public function indexAction()
