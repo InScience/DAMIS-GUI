bffb60bd3b66ed6b021170aace6335ac889dc01a
Gediminas Šaltenis <gediminas@satenis.lt>
2014-03-07 12:41:12 +0200

    multilanguage support for lt and en languages

--- src/Base/MainBundle/Controller/DefaultController.php
+++ src/Base/MainBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Base\MainBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class DefaultController extends Controller
 {
@@ -15,5 +16,32 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return array();
+    }
+
+    /**
+     * @Route("/lt.html", name="change_locale_lt")
+     * @Method("GET")
+     * @Template()
+     */
+    public function localeLtAction()
+    {
+        $this->getRequest()->getSession()->set('_locale', 'lt_LT');
+        $request = $this->getRequest();
+        $locale = $request->getLocale();
+        $request->setLocale('lt_LT');
+        return $this->redirect($this->get('request')->headers->get('referer'));
+    }
+
+    /**
+     * @Route("/en.html", name="change_locale_en")
+     * @Method("GET")
+     * @Template()
+     */
+    public function localeEnAction(){
+        $this->getRequest()->getSession()->set('_locale', 'en_US');
+        $request = $this->getRequest();
+        $locale = $request->getLocale();
+        $request->setLocale('en_US');
+        return $this->redirect($this->get('request')->headers->get('referer'));
     }
 }
---
+++ src/Base/MainBundle/EventListener/LocaleListener.php
@@ -0,0 +1,41 @@
+<?php
+namespace Base\MainBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class LocaleListener implements EventSubscriberInterface
+{
+    private $defaultLocale;
+
+    public function __construct($defaultLocale = 'lt_LT')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        // try to see if the locale has been set as a _locale routing parameter
+        if ($locale = $request->getSession()->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+            $request->setLocale($locale);
+        } else {
+            // if no explicit locale has been set on this request, use one from the session
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            // must be registered before the default Locale listener
+            KernelEvents::REQUEST => array(array('onKernelRequest', 17)),
+        );
+    }
+}
