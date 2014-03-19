ce689e7e292c3bdc38ae414f28ea7be43c424fcc
Karolis Gudiškis <karolisgud@gmail.com>
2014-03-19 13:36:58 +0200

    MLP form validator

--- src/Damis/ExperimentBundle/Form/Type/MlpType.php
+++ src/Damis/ExperimentBundle/Form/Type/MlpType.php
@@ -11,41 +11,94 @@ use Symfony\Component\Validator\Constraints as Assert;
     class MlpType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
+        $dataValidator = function($object, ExecutionContextInterface $context) use ($builder) {
+            $data = $builder->getData();
+            $testData = $data['testData'];
+            $validationData = $data['validationData'];
+            if($object + $testData + $validationData != 100) {
+                $context->addViolation('Sum of data fields should be 100%', [], null);
+            }
+        };
+
         $builder
         ->add('maxIterations', 'number', [
-            'required' => true,
-            'data' => 100,
-            'constraints' => [
-                new Assert\Range([
-                    'min' => 0,
-                    'max' => 1000
-                ]),
-                new NotBlank()
-            ]
+                'required' => true,
+                'data' => 100,
+                'constraints' => [
+                    new Assert\Range([
+                        'min' => 0,
+                        'max' => 1000,
+                        'invalidMessage' => 'Number of iteration must be in interval [1; 1000]'
+                    ]),
+                    new NotBlank()
+                ]
             ])
         ->add('1layer', 'number', [
                 'required' => true,
                 'data' => 5,
+                'constraints' => [
+                    new Assert\GreaterThanOrEqual([
+                        'value' => 1,
+                        'message' => 'Number of hidden neurons at level 1 must be greater than 1'
+                    ]),
+                    new NotBlank()
+                ]
+
             ])
         ->add('2layer', 'number', [
                 'required' => true,
                 'data' => 0,
+                'constraints' => [
+                    new Assert\GreaterThanOrEqual([
+                        'value' => 0,
+                        'message' => 'Number of hidden neurons at level 2, 3 cannot be negative'
+                    ]),
+                    new NotBlank()
+                ]
             ])
         ->add('3layer', 'number', [
-            'required' => true,
-            'data' => 0,
+                'required' => true,
+                'data' => 0,
+                'constraints' => [
+                    new Assert\GreaterThanOrEqual([
+                        'value' => 0,
+                        'message' => 'Number of hidden neurons at level 2, 3 cannot be negative'
+                    ]),
+                    new NotBlank()
+                ]
+
             ])
         ->add('trainingData', 'number', [
                 'required' => true,
                 'data' => 80,
+                'constraints' => [
+                    new Assert\GreaterThanOrEqual([
+                        'value' => 1,
+                    ]),
+                    new NotBlank(),
+                    $dataValidator
+                ]
+
             ])
         ->add('testData', 'number', [
                 'required' => true,
                 'data' => 10,
+                'constraints' => [
+                    new Assert\GreaterThanOrEqual([
+                        'value' => 1,
+                    ]),
+                    new NotBlank()
+                ]
             ])
         ->add('valiadationData', 'number', [
-            'required' => true,
-            'data' => 10,
+                'required' => true,
+                'data' => 10,
+                'constraints' => [
+                    new Assert\GreaterThanOrEqual([
+                        'value' => 1,
+                    ]),
+                    new NotBlank()
+                ]
             ]);
     }
 
