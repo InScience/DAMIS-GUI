8766105eb56a5aa5171c65ec1719f6ff2d618d8c
Karolis Gudiškis <karolisgud@gmail.com>
2014-03-19 13:10:48 +0200

    MLP form

--- src/Damis/ExperimentBundle/Form/Type/MlpType.php
+++ src/Damis/ExperimentBundle/Form/Type/MlpType.php
@@ -11,7 +11,42 @@ use Symfony\Component\Validator\Constraints as Assert;
     class MlpType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
-        $builder->add('maxIterations', 'number');
+        $builder
+        ->add('maxIterations', 'number', [
+            'required' => true,
+            'data' => 100,
+            'constraints' => [
+                new Assert\Range([
+                    'min' => 0,
+                    'max' => 1000
+                ]),
+                new NotBlank()
+            ]
+            ])
+        ->add('1layer', 'number', [
+                'required' => true,
+                'data' => 5,
+            ])
+        ->add('2layer', 'number', [
+                'required' => true,
+                'data' => 0,
+            ])
+        ->add('3layer', 'number', [
+            'required' => true,
+            'data' => 0,
+            ])
+        ->add('trainingData', 'number', [
+                'required' => true,
+                'data' => 80,
+            ])
+        ->add('testData', 'number', [
+                'required' => true,
+                'data' => 10,
+            ])
+        ->add('valiadationData', 'number', [
+            'required' => true,
+            'data' => 10,
+            ]);
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver) {
