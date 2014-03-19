7aafc126c118926362ee8855658a14d3f410a2f8
Deividas Jankauskas <deividasjank@gmail.com>
2014-03-19 15:06:34 +0200

    language change locale bug fix

--- src/Base/MainBundle/EventListener/LocaleListener.php
+++ src/Base/MainBundle/EventListener/LocaleListener.php
@@ -9,9 +9,9 @@ class LocaleListener implements EventSubscriberInterface
 {
     private $defaultLocale;
 
-    public function __construct($defaultLocale = 'lt_LT')
+    public function __construct($defaultLocale = 'lt')
     {
-        $this->defaultLocale = $defaultLocale;
+        $this->defaultLocale = 'lt';
     }
 
     public function onKernelRequest(GetResponseEvent $event)
@@ -23,11 +23,14 @@ class LocaleListener implements EventSubscriberInterface
 
         // try to see if the locale has been set as a _locale routing parameter
         if ($locale = $request->getSession()->get('_locale')) {
+            if($locale == 'lt_LT')
+                $locale = $this->defaultLocale;
             $request->getSession()->set('_locale', $locale);
             $request->setLocale($locale);
         } else {
             // if no explicit locale has been set on this request, use one from the session
             $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+            $request->getSession()->set('_locale', $this->defaultLocale);
         }
     }
 
