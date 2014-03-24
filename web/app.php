52e791b0d4ffd88f72c00937c7ba2d5bc904c7db
Gediminas Šaltenis <gediminas@saltenis.lt>
2014-03-24 15:28:49 +0200

    change cache permissions

--- web/app.php
+++ web/app.php
@@ -3,7 +3,9 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-date_default_timezone_set('Europe/Berlin');
+// If you don't want to setup permissions the proper way, just uncomment the following PHP line
+// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
+umask(0000);
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
