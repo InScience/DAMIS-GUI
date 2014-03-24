b80f413ab6be4ce7b9f4390ef966133839bde929
Gediminas Šaltenis <gediminas@saltenis.lt>
2014-03-24 14:17:55 +0200

    force date time

--- web/app.php
+++ web/app.php
@@ -3,6 +3,8 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
+date_default_timezone_set('Europe/Berlin');
+
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 // Use APC for autoloading to improve performance.
