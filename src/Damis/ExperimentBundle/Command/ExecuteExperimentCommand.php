60819222787f7672b6be32eab7049dd8553f49ff
Gediminas Šaltenis <gediminas@saltenis.lt>
2014-04-03 10:22:01 +0300

    comment testing data

--- src/Damis/ExperimentBundle/Command/ExecuteExperimentCommand.php
+++ src/Damis/ExperimentBundle/Command/ExecuteExperimentCommand.php
@@ -154,7 +154,7 @@ class ExecuteExperimentCommand extends ContainerAwareCommand
             $output->writeln('Wsdl function parameters: ' . print_r($params, true));
 
             //FOR TESTING PURPOSES ONLY
-            $params['X'] = 'http://158.129.140.146/Damis/Data/testData/test.arff';
+            //$params['X'] = 'http://158.129.140.146/Damis/Data/testData/test.arff';
 
             //----------------------------------------------------------------------------------------------------//
             // execute
