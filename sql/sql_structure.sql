bbe37d1c425e9b350557935d2b6076108b5c85e1
Virgis <virgis@kitoks.lt>
2015-04-14 21:42:50 +0300

    Renewed sql files. Structure now contains algorithms table RDF parameters was corrected in data.sql

--- sql/sql_structure.sql
+++ sql/sql_structure.sql
@@ -21,11 +21,10 @@ CREATE TABLE IF NOT EXISTS `cluster` (
   `ClusterName` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
   `ClusterWorkloadHost` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `ClusterDescription` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `ClusterID` int(11) NOT NULL AUTO_INCREMENT,
+  `ClusterID` int(11) NOT NULL,
   `WorkloadUrl` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `ClusterUrl` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`ClusterID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+  `ClusterUrl` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -42,15 +41,11 @@ CREATE TABLE IF NOT EXISTS `component` (
   `ComponentAltDescription` varchar(80) COLLATE utf8_unicode_ci DEFAULT NULL,
   `ComponentLabelLT` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `ComponentLabelEN` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `ComponentID` int(11) NOT NULL AUTO_INCREMENT,
+  `ComponentID` int(11) NOT NULL,
   `FormType` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `ClusterID` int(11) DEFAULT NULL,
-  `ComponentTypeID` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ComponentID`),
-  UNIQUE KEY `COMPONENT_PK` (`ComponentID`),
-  KEY `IDX_49FEA157B26192BE` (`ClusterID`),
-  KEY `IDX_49FEA1576ACDD642` (`ComponentTypeID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=47 ;
+  `ComponentTypeID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -60,10 +55,8 @@ CREATE TABLE IF NOT EXISTS `component` (
 
 CREATE TABLE IF NOT EXISTS `componenttype` (
   `ComponentType` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
-  `ComponentTypeID` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`ComponentTypeID`),
-  UNIQUE KEY `COMPONENTTYPE_PK` (`ComponentTypeID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
+  `ComponentTypeID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -79,13 +72,10 @@ CREATE TABLE IF NOT EXISTS `dataset` (
   `DatasetUpdated` int(11) DEFAULT NULL,
   `file` longtext COLLATE utf8_unicode_ci COMMENT '(DC2Type:array)',
   `DatasetDescription` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `DatasetID` int(11) NOT NULL AUTO_INCREMENT,
+  `DatasetID` int(11) NOT NULL,
   `Hidden` int(11) DEFAULT NULL,
-  `UserID` int(11) DEFAULT NULL,
-  PRIMARY KEY (`DatasetID`),
-  UNIQUE KEY `DATASET_PK` (`DatasetID`),
-  KEY `IDX_B7A041D058746832` (`UserID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `UserID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -94,16 +84,15 @@ CREATE TABLE IF NOT EXISTS `dataset` (
 --
 
 CREATE TABLE IF NOT EXISTS `entity_log` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `action` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
   `logged_at` datetime NOT NULL,
   `object_id` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
   `object_class` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `version` int(11) NOT NULL,
   `data` longtext COLLATE utf8_unicode_ci COMMENT '(DC2Type:array)',
-  `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -120,14 +109,10 @@ CREATE TABLE IF NOT EXISTS `experiment` (
   `ExperimentUsePrimaryMemory` int(11) DEFAULT NULL,
   `ExperimentUseSecMemory` int(11) DEFAULT NULL,
   `ExperimentGUIData` longtext COLLATE utf8_unicode_ci,
-  `ExperimentID` int(11) NOT NULL AUTO_INCREMENT,
+  `ExperimentID` int(11) NOT NULL,
   `ExperimentStatusID` int(11) DEFAULT NULL,
-  `UserID` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ExperimentID`),
-  UNIQUE KEY `EXPERIMENT_PK` (`ExperimentID`),
-  KEY `IDX_136F58B234472C01` (`ExperimentStatusID`),
-  KEY `IDX_136F58B258746832` (`UserID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `UserID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -137,10 +122,8 @@ CREATE TABLE IF NOT EXISTS `experiment` (
 
 CREATE TABLE IF NOT EXISTS `experimentstatus` (
   `ExperimentStatus` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
-  `ExperimentStatusID` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`ExperimentStatusID`),
-  UNIQUE KEY `EXPERIMENTSTATUS_PK` (`ExperimentStatusID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `ExperimentStatusID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -149,7 +132,7 @@ CREATE TABLE IF NOT EXISTS `experimentstatus` (
 --
 
 CREATE TABLE IF NOT EXISTS `page` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `slug` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
   `groupName` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -157,10 +140,8 @@ CREATE TABLE IF NOT EXISTS `page` (
   `position` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL,
-  `language` enum('lt','en') COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UNIQ_140AB620989D9B62` (`slug`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `language` enum('lt','en') COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -175,18 +156,13 @@ CREATE TABLE IF NOT EXISTS `parameter` (
   `ParameterDescription` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
   `ParameterLabelLT` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `ParameterLabelEN` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `ParameterID` int(11) NOT NULL AUTO_INCREMENT,
+  `ParameterID` int(11) NOT NULL,
   `ParameterSlug` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `ParameterPosition` int(11) DEFAULT NULL,
   `ParameterTypeID` int(11) DEFAULT NULL,
   `ParameterConnectionTypeID` int(11) DEFAULT NULL,
-  `ComponentID` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ParameterID`),
-  UNIQUE KEY `PARAMETER_PK` (`ParameterID`),
-  KEY `IDX_2A979110D50E99E0` (`ParameterTypeID`),
-  KEY `IDX_2A97911010C17FF9` (`ParameterConnectionTypeID`),
-  KEY `IDX_2A979110C364FDFE` (`ComponentID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=191 ;
+  `ComponentID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -196,10 +172,8 @@ CREATE TABLE IF NOT EXISTS `parameter` (
 
 CREATE TABLE IF NOT EXISTS `parameterconnectiontype` (
   `ParameterConnectionType` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
-  `ParameterConnectionTypeID` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`ParameterConnectionTypeID`),
-  UNIQUE KEY `PARAMETERCONNECTIONTYPE_PK` (`ParameterConnectionTypeID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+  `ParameterConnectionTypeID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -209,10 +183,8 @@ CREATE TABLE IF NOT EXISTS `parameterconnectiontype` (
 
 CREATE TABLE IF NOT EXISTS `parametertype` (
   `ParameterType` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
-  `ParameterTypeID` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`ParameterTypeID`),
-  UNIQUE KEY `PARAMETERTYPE_PK` (`ParameterTypeID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `ParameterTypeID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -222,14 +194,10 @@ CREATE TABLE IF NOT EXISTS `parametertype` (
 
 CREATE TABLE IF NOT EXISTS `parametervalue` (
   `ParameterValue` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `ParameterValueID` int(11) NOT NULL AUTO_INCREMENT,
+  `ParameterValueID` int(11) NOT NULL,
   `WorkflowTaskID` int(11) DEFAULT NULL,
-  `ParameterID` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ParameterValueID`),
-  UNIQUE KEY `PARAMETERVALUE_PK` (`ParameterValueID`),
-  KEY `IDX_EF5C2B2A199F0DB9` (`WorkflowTaskID`),
-  KEY `IDX_EF5C2B2A5A8577F9` (`ParameterID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `ParameterID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -239,9 +207,25 @@ CREATE TABLE IF NOT EXISTS `parametervalue` (
 
 CREATE TABLE IF NOT EXISTS `pvalueoutpvaluein` (
   `InParameterValueID` int(11) NOT NULL,
-  `OutParameterValueID` int(11) DEFAULT NULL,
-  PRIMARY KEY (`InParameterValueID`),
-  KEY `IDX_A522F8AE88C1F20` (`OutParameterValueID`)
+  `OutParameterValueID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `useralgorithm`
+--
+
+CREATE TABLE IF NOT EXISTS `useralgorithm` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `file_title` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
+  `file_created` int(11) NOT NULL,
+  `file_path` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `file_updated` int(11) DEFAULT NULL,
+  `file` longtext COLLATE utf8_unicode_ci COMMENT '(DC2Type:array)',
+  `file_description` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `hidden` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -251,7 +235,7 @@ CREATE TABLE IF NOT EXISTS `pvalueoutpvaluein` (
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `username_canonical` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -272,11 +256,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `surname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `organisation` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `user_id` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UNIQ_1483A5E992FC23A8` (`username_canonical`),
-  UNIQUE KEY `UNIQ_1483A5E9A0D96FBF` (`email_canonical`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+  `user_id` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -286,36 +267,219 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 CREATE TABLE IF NOT EXISTS `workflowtask` (
   `WorkflowTaskIsRunning` int(11) NOT NULL,
-  `WorkflowTaskID` int(11) NOT NULL AUTO_INCREMENT,
+  `WorkflowTaskID` int(11) NOT NULL,
   `TaskBox` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
   `Message` longtext COLLATE utf8_unicode_ci,
   `ExecutionTime` double DEFAULT NULL,
-  `ExperimentID` int(11) DEFAULT NULL,
-  PRIMARY KEY (`WorkflowTaskID`),
-  UNIQUE KEY `WORKFLOWTASK_PK` (`WorkflowTaskID`),
-  KEY `IDX_5F598CF2BAA1BE51` (`ExperimentID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `ExperimentID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Table structure for table `useralgorithm`
+-- Indexes for dumped tables
 --
 
-CREATE TABLE IF NOT EXISTS `useralgorithm` (
-`id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `file_title` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
-  `file_created` int(11) NOT NULL,
-  `file_path` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `file_updated` int(11) DEFAULT NULL,
-  `file` longtext COLLATE utf8_unicode_ci COMMENT '(DC2Type:array)',
-  `file_description` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `hidden` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `USER_ALGORITHM_FILE_PK` (`id`),
-  KEY `IDX_5B486DD9A76ED395` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
+--
+-- Indexes for table `cluster`
+--
+ALTER TABLE `cluster`
+  ADD PRIMARY KEY (`ClusterID`);
 
+--
+-- Indexes for table `component`
+--
+ALTER TABLE `component`
+  ADD PRIMARY KEY (`ComponentID`),
+  ADD UNIQUE KEY `COMPONENT_PK` (`ComponentID`),
+  ADD KEY `IDX_49FEA157B26192BE` (`ClusterID`),
+  ADD KEY `IDX_49FEA1576ACDD642` (`ComponentTypeID`);
 
+--
+-- Indexes for table `componenttype`
+--
+ALTER TABLE `componenttype`
+  ADD PRIMARY KEY (`ComponentTypeID`),
+  ADD UNIQUE KEY `COMPONENTTYPE_PK` (`ComponentTypeID`);
+
+--
+-- Indexes for table `dataset`
+--
+ALTER TABLE `dataset`
+  ADD PRIMARY KEY (`DatasetID`),
+  ADD UNIQUE KEY `DATASET_PK` (`DatasetID`),
+  ADD KEY `IDX_B7A041D058746832` (`UserID`);
+
+--
+-- Indexes for table `entity_log`
+--
+ALTER TABLE `entity_log`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `experiment`
+--
+ALTER TABLE `experiment`
+  ADD PRIMARY KEY (`ExperimentID`),
+  ADD UNIQUE KEY `EXPERIMENT_PK` (`ExperimentID`),
+  ADD KEY `IDX_136F58B234472C01` (`ExperimentStatusID`),
+  ADD KEY `IDX_136F58B258746832` (`UserID`);
+
+--
+-- Indexes for table `experimentstatus`
+--
+ALTER TABLE `experimentstatus`
+  ADD PRIMARY KEY (`ExperimentStatusID`),
+  ADD UNIQUE KEY `EXPERIMENTSTATUS_PK` (`ExperimentStatusID`);
+
+--
+-- Indexes for table `page`
+--
+ALTER TABLE `page`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_140AB620989D9B62` (`slug`);
+
+--
+-- Indexes for table `parameter`
+--
+ALTER TABLE `parameter`
+  ADD PRIMARY KEY (`ParameterID`),
+  ADD UNIQUE KEY `PARAMETER_PK` (`ParameterID`),
+  ADD KEY `IDX_2A979110D50E99E0` (`ParameterTypeID`),
+  ADD KEY `IDX_2A97911010C17FF9` (`ParameterConnectionTypeID`),
+  ADD KEY `IDX_2A979110C364FDFE` (`ComponentID`);
+
+--
+-- Indexes for table `parameterconnectiontype`
+--
+ALTER TABLE `parameterconnectiontype`
+  ADD PRIMARY KEY (`ParameterConnectionTypeID`),
+  ADD UNIQUE KEY `PARAMETERCONNECTIONTYPE_PK` (`ParameterConnectionTypeID`);
+
+--
+-- Indexes for table `parametertype`
+--
+ALTER TABLE `parametertype`
+  ADD PRIMARY KEY (`ParameterTypeID`),
+  ADD UNIQUE KEY `PARAMETERTYPE_PK` (`ParameterTypeID`);
+
+--
+-- Indexes for table `parametervalue`
+--
+ALTER TABLE `parametervalue`
+  ADD PRIMARY KEY (`ParameterValueID`),
+  ADD UNIQUE KEY `PARAMETERVALUE_PK` (`ParameterValueID`),
+  ADD KEY `IDX_EF5C2B2A199F0DB9` (`WorkflowTaskID`),
+  ADD KEY `IDX_EF5C2B2A5A8577F9` (`ParameterID`);
+
+--
+-- Indexes for table `pvalueoutpvaluein`
+--
+ALTER TABLE `pvalueoutpvaluein`
+  ADD PRIMARY KEY (`InParameterValueID`),
+  ADD KEY `IDX_A522F8AE88C1F20` (`OutParameterValueID`);
+
+--
+-- Indexes for table `useralgorithm`
+--
+ALTER TABLE `useralgorithm`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `USER_ALGORITHM_FILE_PK` (`id`),
+  ADD KEY `IDX_5B486DD9A76ED395` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_1483A5E992FC23A8` (`username_canonical`),
+  ADD UNIQUE KEY `UNIQ_1483A5E9A0D96FBF` (`email_canonical`);
+
+--
+-- Indexes for table `workflowtask`
+--
+ALTER TABLE `workflowtask`
+  ADD PRIMARY KEY (`WorkflowTaskID`),
+  ADD UNIQUE KEY `WORKFLOWTASK_PK` (`WorkflowTaskID`),
+  ADD KEY `IDX_5F598CF2BAA1BE51` (`ExperimentID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cluster`
+--
+ALTER TABLE `cluster`
+  MODIFY `ClusterID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `component`
+--
+ALTER TABLE `component`
+  MODIFY `ComponentID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `componenttype`
+--
+ALTER TABLE `componenttype`
+  MODIFY `ComponentTypeID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `dataset`
+--
+ALTER TABLE `dataset`
+  MODIFY `DatasetID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `entity_log`
+--
+ALTER TABLE `entity_log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `experiment`
+--
+ALTER TABLE `experiment`
+  MODIFY `ExperimentID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `experimentstatus`
+--
+ALTER TABLE `experimentstatus`
+  MODIFY `ExperimentStatusID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `page`
+--
+ALTER TABLE `page`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `parameter`
+--
+ALTER TABLE `parameter`
+  MODIFY `ParameterID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `parameterconnectiontype`
+--
+ALTER TABLE `parameterconnectiontype`
+  MODIFY `ParameterConnectionTypeID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `parametertype`
+--
+ALTER TABLE `parametertype`
+  MODIFY `ParameterTypeID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `parametervalue`
+--
+ALTER TABLE `parametervalue`
+  MODIFY `ParameterValueID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `useralgorithm`
+--
+ALTER TABLE `useralgorithm`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `workflowtask`
+--
+ALTER TABLE `workflowtask`
+  MODIFY `WorkflowTaskID` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
@@ -363,16 +527,16 @@ ALTER TABLE `pvalueoutpvaluein`
   ADD CONSTRAINT `FK_A522F8AEF169B8E2` FOREIGN KEY (`InParameterValueID`) REFERENCES `parametervalue` (`ParameterValueID`) ON DELETE CASCADE;
 
 --
--- Constraints for table `workflowtask`
---
-ALTER TABLE `workflowtask`
-  ADD CONSTRAINT `FK_5F598CF2BAA1BE51` FOREIGN KEY (`ExperimentID`) REFERENCES `experiment` (`ExperimentID`) ON DELETE CASCADE;
-
---
 -- Constraints for table `useralgorithm`
 --
 ALTER TABLE `useralgorithm`
   ADD CONSTRAINT `FK_5B486DD9A76ED395` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `workflowtask`
+--
+ALTER TABLE `workflowtask`
+  ADD CONSTRAINT `FK_5F598CF2BAA1BE51` FOREIGN KEY (`ExperimentID`) REFERENCES `experiment` (`ExperimentID`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
