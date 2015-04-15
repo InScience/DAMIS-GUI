2a491ecb51af8921702cf32b4eefbd1f004b8fd9
Virgis <virgis@kitoks.lt>
2015-04-15 12:15:45 +0300

    Sql updates after v0.1.1

--- sql/update.sql
+++ sql/update.sql
@@ -1,7 +1,7 @@
 -- File for templorary updates
 
---- Updates after 2014-04-14  Version 0.1.1
+-- Updates after 2014-04-14  Version 0.1.1
 
---- Filter data component update
+-- Filter data component update
 UPDATE `damis`.`component` SET `ComponentDescription` = 'Filter data description' WHERE `component`.`ComponentID` = 9; 
 UPDATE `damis`.`component` SET `ComponentDescription` = 'Filter data description' WHERE `component`.`ComponentID` = 10;
